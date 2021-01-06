795ec1e46353a3ea9838a8139ecc558db5f890c3
Stepan Pittauer <76156390+AspiqBoy@users.noreply.github.com>
2021-01-06 13:17:45 +0100

    Update component.py

--- src/component.py
+++ src/component.py
@@ -18,11 +18,8 @@ KEY_PRINT_HELLO = 'print_hello'
 KEY_DEBUG = 'debug'
 
 # list of mandatory parameters => if some is missing, component will fail with readable message on initialization.
-MANDATORY_PARS = [KEY_DEBUG]
+MANDATORY_PARS = []
 MANDATORY_IMAGE_PARS = []
-
-APP_VERSION = '0.0.1'
-
 
 class Component(KBCEnvHandler):
 
@@ -31,24 +28,26 @@ class Component(KBCEnvHandler):
         default_data_dir = Path(__file__).resolve().parent.parent.joinpath('data').as_posix() \
             if not os.environ.get('KBC_DATADIR') else None
 
-        KBCEnvHandler.__init__(self, MANDATORY_PARS, log_level=logging.DEBUG if debug else logging.INFO,
-                               data_path=default_data_dir)
+        KBCEnvHandler.__init__(
+            self,
+            MANDATORY_PARS,
+            log_level=logging.DEBUG if debug else logging.INFO,
+            data_path=default_data_dir
+        )
+
         # override debug from config
         if self.cfg_params.get(KEY_DEBUG):
-            debug = True
-        if debug:
             logging.getLogger().setLevel(logging.DEBUG)
-        logging.info('Running version %s', APP_VERSION)
+
         logging.info('Loading configuration...')
 
         try:
-            # validation of mandatory parameters. Produces ValueError
             self.validate_config(MANDATORY_PARS)
             self.validate_image_parameters(MANDATORY_IMAGE_PARS)
         except ValueError as e:
             logging.exception(e)
             exit(1)
-
+            
     def run(self):
 
         # get config params
